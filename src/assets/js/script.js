74f4a636f03e75f5ca257bfb254baac8e75e6c09
eapil16 <77381727+eapil16@users.noreply.github.com>
2023-04-06 15:00:00 +0300

    add

---
+++ src/assets/js/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	$('.menu-toggle-cont').click(function (e) {
+   e.preventDefault();
+    if (!$(this).hasClass('menu-toggle-cont_active')) { 
+      $(this).addClass('menu-toggle-cont_active'); 
+      $('.fixed-menu').slideDown(300); 
+      $('body').addClass('hidd'); 
+    } else { 
+      $(this).removeClass('menu-toggle-cont_active'); 
+      $('.fixed-menu').slideUp(300);
+      $('body').removeClass('hidd');
+    } 
+  }); 
+
+
+  // $('.sidebar-button').click(function (e) {
+  //   // $(this).toggleClass('active'); 
+  //   $('.sidebar').slideToggle(); 
+  // });
+  
+  
+});
